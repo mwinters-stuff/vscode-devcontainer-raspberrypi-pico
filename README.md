f662b4ac8c94eebdf7a30ad166d2b8129b08818d
Mathew Winters <mathew@winters.org.nz>
2021-06-06 07:20:47 +0000

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # vscode-devcontainer-raspberrypi-pico
 VSCode Dev Container for the Raspberry PI Pico C SDK
+
+## Usage
+
+Clone or download repository, copy the contents of .devcontainer into your project
+Remove any existing "build" directory.
+
+Open the folder, and allow the container to build.
+
+# To know
+* SDK is installed in /pico-sdk
+* All USB devices are exported to container - so the picoprobe can be found and used.
+* USB Device /dev/ttyACM0 is exported to container for monitoring
+
+## This container includes
+* pico-sdk
+* openocd - compiled for picoprobe
+* picotool
+* bootterm (for serial monitoring) - see https://github.com/wtarreau/bootterm
+
---
+++ TestProject.cpp
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include "pico/stdlib.h"
+#include "hardware/spi.h"
+#include "hardware/i2c.h"
+#include "hardware/dma.h"
+#include "hardware/clocks.h"
+
+// SPI Defines
+// We are going to use SPI 0, and allocate it to the following GPIO pins
+// Pins can be changed, see the GPIO function select table in the datasheet for information on GPIO assignments
+#define SPI_PORT spi0
+#define PIN_MISO 16
+#define PIN_CS 17
+#define PIN_SCK 18
+#define PIN_MOSI 19
+
+// I2C defines
+// This example will use I2C0 on GPIO8 (SDA) and GPIO9 (SCL) running at 400KHz.
+// Pins can be changed, see the GPIO function select table in the datasheet for information on GPIO assignments
+#define I2C_PORT i2c0
+#define I2C_SDA 8
+#define I2C_SCL 9
+
+int main()
+{
+  stdio_init_all();
+
+  // SPI initialisation. This example will use SPI at 1MHz.
+  spi_init(SPI_PORT, 1000 * 1000);
+  gpio_set_function(PIN_MISO, GPIO_FUNC_SPI);
+  gpio_set_function(PIN_CS, GPIO_FUNC_SIO);
+  gpio_set_function(PIN_SCK, GPIO_FUNC_SPI);
+  gpio_set_function(PIN_MOSI, GPIO_FUNC_SPI);
+
+  // Chip select is active-low, so we'll initialise it to a driven-high state
+  gpio_set_dir(PIN_CS, GPIO_OUT);
+  gpio_put(PIN_CS, 1);
+
+  // I2C Initialisation. Using it at 400Khz.
+  i2c_init(I2C_PORT, 400 * 1000);
+
+  gpio_set_function(I2C_SDA, GPIO_FUNC_I2C);
+  gpio_set_function(I2C_SCL, GPIO_FUNC_I2C);
+  gpio_pull_up(I2C_SDA);
+  gpio_pull_up(I2C_SCL);
+
+  puts("Hello, world!");
+
+  const uint LED_PIN = PICO_DEFAULT_LED_PIN;
+  gpio_init(LED_PIN);
+  gpio_set_dir(LED_PIN, GPIO_OUT);
+  int delay=100;
+  while (true)
+  {
+    printf("Delay %d\n",delay);
+    gpio_put(LED_PIN, 1);
+    sleep_ms(delay);
+    gpio_put(LED_PIN, 0);
+
+    sleep_ms(100);
+    delay += 100;
+    if(delay > 2000){
+      delay = 100;
+    }
+  }
+
+  return 0;
+}
